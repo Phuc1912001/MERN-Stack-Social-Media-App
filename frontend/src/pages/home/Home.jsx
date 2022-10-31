a0a0fdc35b4cbd754fc13f223948bef4632535f5
Phuc1912001 <phucphuc1912001@gmail.com>
2022-10-31 19:56:17 +0700

    PostShare

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import PostSide from "../../components/PostSide/PostSide";
 import ProfileSide from "../../components/profileSide/ProfileSide";
-import './Home.css'
+import RightSide from "../../components/RightSide/RightSide";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="Home">
-      <ProfileSide/>
-      <div className="potsSide"> Posts </div>
-      <div className="RightSide">Rightside</div>
+      <ProfileSide />
+      <PostSide />
+      <RightSide/>
     </div>
   );
 };
