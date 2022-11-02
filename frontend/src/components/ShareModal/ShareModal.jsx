120818002c6f202d8f4fced3822ffe4fc7e9a6ec
Phuc1912001 <phucphuc1912001@gmail.com>
2022-11-02 15:22:08 +0700

    setup môi trường backend

--- frontend/src/components/ShareModal/ShareModal.jsx
+++ frontend/src/components/ShareModal/ShareModal.jsx
@@ -1,27 +1,27 @@
 
-// import { Modal, useMantineTheme } from "@mantine/core";
+import { Modal, useMantineTheme } from "@mantine/core";
 
-// import PostShare from "../PostShare/PostShare";
+import PostShare from "../PostShare/PostShare";
 
-// function ShareModal({ modalOpened, setModalOpened }) {
-//   const theme = useMantineTheme();
+function ShareModal({ modalOpened, setModalOpened }) {
+  const theme = useMantineTheme();
 
-//   return (
-//     <Modal
-//       overlayColor={
-//         theme.colorScheme === "dark"
-//           ? theme.colors.dark[9]
-//           : theme.colors.gray[2]
-//       }
-//       overlayOpacity={0.55}
-//       overlayBlur={3}
-//       size="55%"
-//       opened={modalOpened}
-//       onClose={() => setModalOpened(false)}
-//     >
-//       <PostShare />
-//     </Modal>
-//   );
-// }
+  return (
+    <Modal
+      overlayColor={
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[9]
+          : theme.colors.gray[2]
+      }
+      overlayOpacity={0.55}
+      overlayBlur={3}
+      size="55%"
+      opened={modalOpened}
+      onClose={() => setModalOpened(false)}
+    >
+      <PostShare />
+    </Modal>
+  );
+}
 
-// export default ShareModal;
+export default ShareModal;
